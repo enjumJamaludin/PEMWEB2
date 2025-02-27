3bf2239d8264e6a8f05ff21ac73e3d21ef531464
enjumJamaludin <enjumjamaludin359@gmail.com>
2025-02-27 21:16:55 +0700

    Add files via upload

---
+++ PERTEMUAN 1/welcome.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WELCOME</title>
+</head> 
+<body>
+    <h3>Selamat Belajar Web</h3>
+    <?php
+        $_nama = "Budi Darmawan";
+        $_umur = 20;
+        echo "Nama saya $_nama, umur saya $_umur tahun";
+        echo "<br>";
+    ?>
+
+</body>
+</html>      
